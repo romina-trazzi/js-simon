fc227e33db6acaa9ef9e8aaa7f83504ff508ad70
Romina Trazzi <romina.trazzi@posta.it>
2020-12-02 06:09:53 +0100

    Commit set Timeout
    Generato il counter con i prompt e salvati i numeri utente in un array

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,43 +7,53 @@
 Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati. */
 
-// Generiamo 5 numeri casuali tramite una funzione + ciclo e li inseriamo in un Array
+
+
+
+// Generiamo 5 numeri casuali tramite una funzione + ciclo e li inseriamo in un array
 
 $(function () {
 
-function numeroRandom(min, max) {
-    var numero = (Math.floor(Math.random() * max + min));
-    // console.log(numero);
-    return numero;
-} 
+    function numeroRandom(min, max) {
+        var numero = (Math.floor(Math.random() * max + min));
+        // console.log(numero);
+        return numero;
+    } 
 
-var numeriInMemoria = [];
+    var numeriInMemoria = [];
 
-for (i = 0; i < 5; i++) {
-    var arrayPc = numeroRandom(0, 100);
-    numeriInMemoria.push(arrayPc);
-    var risultato = document.getElementById("result");
-    risultato.innerHTML = numeriInMemoria;
-} 
+    for (i = 0; i < 5; i++) {
+        var arrayPc = numeroRandom(0, 100);
+        numeriInMemoria.push(arrayPc);
+        var risultato = document.getElementById("result");
+        risultato.innerHTML = numeriInMemoria;
+    } 
 
-// console.log(numeriInMemoria);
+    // console.log(numeriInMemoria);
 
-// Impostiamo dopo 30 secondi i 5 prompt con cui l'utente inserirà i numeri ricordati
-var numeriMemorizzati = setTimeout(typePrompt, 30000);
-  
-   
-   var arrayUtente = [];
+    /* Impostiamo un timer di 30 secondi dopo il quale i numeri generati dal 
+    computer spariranno, ed appariranno in successione i 5 prompt.
+    Salviamo i numeri dell'utente in un secondo array. */
+
+    var myClock = setTimeout(typePrompt, 3000);
 
     function typePrompt() {
+        risultato.innerHTML = "";
+        var arrayUtente = [];
+        
         for (i = 0; i < 5; i++) {
-            var numeriUtente = (prompt("Inserisci i numeri che ricordi in ordine casuale"));
+            var numeriUtente = Number((prompt("Inserisci i numeri che ricordi in ordine casuale")));
             arrayUtente.push(numeriUtente);
-            return numeriUtente;        
+            var risultatoUtente = document.getElementById("user_result");
+            risultatoUtente.innerHTML = arrayUtente;  
         }
+        // console.log(arrayUtente);
+        return arrayUtente;     
     }
 
-clearTimeout(numeriMemorizzati);
 
-typePrompt(); 
+
+
+
 
 });
